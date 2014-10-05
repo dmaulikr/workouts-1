8a529077200ac8ae5212f1ed92aa1aeb20fec0e3
Luke Regan <LReganCG@gmail.com>
2014-10-04 23:24:58 -0400

    visuals layed out and ready for functionality

---
+++ ExCy/WorkoutLogViewController.swift
@@ -0,0 +1,35 @@
+//
+//  WorkoutLogViewController.swift
+//  ExCy
+//
+//  Created by Luke Regan on 10/4/14.
+//  Copyright (c) 2014 LukeRegan. All rights reserved.
+//
+
+import UIKit
+
+class WorkoutLogViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue!, sender: AnyObject!) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
