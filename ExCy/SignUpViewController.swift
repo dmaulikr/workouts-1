88a94f4a52744b6c4a7fb108e7639792178a3cb0
Luke Regan <luke.regan@mobilesmith.com>
2017-02-13 14:24:12 -0500

    easier sign in process

--- ExCy/SignUpViewController.swift
+++ ExCy/SignUpViewController.swift
@@ -7,37 +7,13 @@
 //
 
 import UIKit
-import Parse
 import Firebase
 import FirebaseAuth
-// FIXME: comparison operators with optionals were removed from the Swift Standard Libary.
-// Consider refactoring the code to use the non-optional operators.
-fileprivate func < <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
-  switch (lhs, rhs) {
-  case let (l?, r?):
-    return l < r
-  case (nil, _?):
-    return true
-  default:
-    return false
-  }
-}
-
-// FIXME: comparison operators with optionals were removed from the Swift Standard Libary.
-// Consider refactoring the code to use the non-optional operators.
-fileprivate func > <T : Comparable>(lhs: T?, rhs: T?) -> Bool {
-  switch (lhs, rhs) {
-  case let (l?, r?):
-    return l > r
-  default:
-    return rhs < lhs
-  }
-}
 
 
 let KEY_UID = "uid"
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController, UITextFieldDelegate {
 	
 	@IBOutlet weak var genderSelection: UISegmentedControl!
 	@IBOutlet weak var ageTextField: UITextField!
@@ -47,30 +23,64 @@ class SignUpViewController: UIViewController {
 	@IBOutlet weak var usernameTextField: UITextField!
 	@IBOutlet weak var passwordTextField: UITextField!
 
+    var TextFieldArray: [UITextField: Int] = [:]
+    var keyboardHeight: CGFloat?
+    var keyboardVisible = false
 	
+    @IBOutlet weak var textfieldBottomLayoutContstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var textfieldTopLayoutContstraint: NSLayoutConstraint!
+    
 	var activityIndicator:UIActivityIndicatorView = UIActivityIndicatorView()
 	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(SignUpViewController.DismissKeyboard))
 		view.addGestureRecognizer(tap)
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow(notification:)), name: NSNotification.Name.UIKeyboardWillShow, object: nil)
+        
+//        NotificationCenter.default.addObserver(self, selector: #selector(textFieldShouldBeginEditing(notification:)), name: NSNotification.Name.UITextFieldTextDidBeginEditing , object: nil)
+//        
+        self.TextFieldArray = [weightTextField: 1, heightTextField: 2, ageTextField: 3, usernameTextField: 4, emailAddressTextField: 5, passwordTextField: 6]
 		
 	}
+    
+//    func textFieldShouldBeginEditing(notification: NSNotification) {
+//        
+//    }
+    
+    func keyboardWillShow(notification: NSNotification) {
+        if let info = notification.userInfo {
+            let rect: CGRect = info["UIKeyboardFrameEndUserInfoKey"] as! CGRect
+            keyboardHeight = rect.height
+            keyboardVisible = true
+            
+            self.view.layoutIfNeeded()
+            UIView.animate(withDuration: 0.25, animations: { 
+                self.view.layoutIfNeeded()
+                self.textfieldBottomLayoutContstraint.constant = rect.height - 30
+                self.textfieldTopLayoutContstraint.constant = -rect.height + 30
+            })
+        }
+    }
 	
 	
 	func checkIfFieldsFilled() -> (success: Bool, message: String) {
-		guard ageTextField.text?.characters.count > 0 else { return (false, "Please enter age") }
-		guard heightTextField.text?.characters.count > 0 else { return (false, "Please enter height") }
-		guard weightTextField.text?.characters.count > 0 else { return (false, "Please enter weight") }
-		guard emailAddressTextField.text?.characters.count > 0 else { return (false, "Please enter email address") }
-		guard usernameTextField.text?.characters.count > 0 else { return (false, "Please enter username") }
-		guard passwordTextField.text?.characters.count > 0 else { return (false, "Please enter password") }
+		guard ageTextField.text!.characters.count > 0 else { return (false, "Please enter age") }
+		guard heightTextField.text!.characters.count > 0 else { return (false, "Please enter height") }
+		guard weightTextField.text!.characters.count > 0 else { return (false, "Please enter weight") }
+		guard emailAddressTextField.text!.characters.count > 0 else { return (false, "Please enter email address") }
+		guard usernameTextField.text!.characters.count > 0 else { return (false, "Please enter username") }
+		guard passwordTextField.text!.characters.count > 0 else { return (false, "Please enter password") }
 		return (true, "")
 	}
 	
 	override func viewDidAppear(_ animated: Bool) {
 		
 	}
+
+    
 
 	@IBAction func SignUpButtonPressed(_ sender: UIButton) {
 		
@@ -132,7 +142,14 @@ class SignUpViewController: UIViewController {
 	}
 	func DismissKeyboard(){
 		//Causes the view (or one of its embedded text fields) to resign the first responder status.
+        if let keyboardHeight = keyboardHeight {
+            if keyboardVisible {
+                self.textfieldBottomLayoutContstraint.constant -= keyboardHeight + 30
+                self.textfieldTopLayoutContstraint.constant += keyboardHeight - 30
+            }
+        }
 		view.endEditing(true)
+        keyboardVisible = false
 	}
 
 	
